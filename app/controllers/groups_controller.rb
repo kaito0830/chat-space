82dfbc8b2e8a98ea323d69d0eb8b372d48d72da4
kaito0830 <kaitobad830@outlook.jp>
2020-03-06 19:00:43 +0900

    final check

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -32,6 +34,11 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids:[])
+  end
+
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
